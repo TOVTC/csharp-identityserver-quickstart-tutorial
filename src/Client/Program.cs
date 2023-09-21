56b4fcde9e9cfaf594f3d9d632512a1328d703eb
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-21 17:11:37 -0400

    added code to call api from client and print authorization token

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityModel.Client;
 using System.Net.Sockets;
+using System.Text.Json.Nodes;
 
 namespace Client
 {
@@ -37,6 +38,22 @@ namespace Client
             }
 
             Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JsonArray.Parse(content));
+            }
         }
     }
 }
