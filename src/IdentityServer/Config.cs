e8bb71c5065dd8f71d45fe621d370a3e5fed607b
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-21 12:12:44 -0400

    defined API scope and defined client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -9,18 +9,33 @@ namespace IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
             {
-                new IdentityResources.OpenId()
+                new ApiScope("api1", "My Api")
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
-
         public static IEnumerable<Client> Clients =>
-            new Client[]
-            { };
+            new List<Client>
+            {
+                // the ClientId and ClientSecret can be thought of as the login and password for the application itself
+                // it identifies the application to the identity server so that it knows which application is trying to connect to it
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
     }
 }
