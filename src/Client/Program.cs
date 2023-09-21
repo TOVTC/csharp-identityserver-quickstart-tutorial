878e95b9a5c1ca8d37f6d20ea59986b3af65aabd
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-21 16:32:32 -0400

    added client

---
+++ src/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using IdentityModel.Client;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            // IdentityModel includes a client library to use with the discovery endpoint so only the base-address of the IdentityServer is required
+            // the actual endpoint addresses can be read from the metadata
+            
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
