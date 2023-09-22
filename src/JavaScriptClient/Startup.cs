7f7a41219445d52772a5b31f5a4673c98e1191c9
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-22 18:17:18 -0400

    configured app with middleware to server static files from wwwroot and added oidc-client

---
+++ src/JavaScriptClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace JavaScriptClient
+{
+    public class Startup
+    {
+        // middleware to serve static files from the wwwroot folder
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
